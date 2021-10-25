de34f14005766ed57c2bab0a4036e9e53a771420
unknown <claudiaclrduro@gmail.com>
2021-10-25 11:15:56 +0200

    firt commit

---
+++ practica.js
@@ -0,0 +1,25 @@
+//  Desarrollar una función que escriba en la consola del navegador
+// Cada item de una lista de la compra
+
+
+listaCompra = [ "patatas", "huevos", "cebollas", "calabacin",
+  "aguacate"]
+
+listaCosas = [ "martillo", "maderas", "clavo"]
+
+imprimirEnConsola ();
+function imprimirEnConsola () {
+    for (let i=0; i<listaCosas.lenght; i++) {
+        console.log (listaCosas[i])
+    }
+}
+   
+   
+function imprimirEnConsola () {
+    for (let i=0; i<listaCompra.lenght; i++) {
+        console.log (listaCompra[i])
+    }
+}
+   
+   
+
